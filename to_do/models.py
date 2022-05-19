646bb8854549318a5533f2b917e4cd19f28084c8
Justin Goncalves <jvgoncalves85@gmail.com>
2022-05-19 18:27:47 -0400

    added class to to_do app + made migrations

--- to_do/models.py
+++ to_do/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class To_Do(models.Model):
+    name = models.CharField(max_length=100)
+    completed = models.BooleanField(default=False)
+    
