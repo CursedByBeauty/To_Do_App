aafe79098996dba422e71b1013134a01c517a407
Justin Goncalves <jvgoncalves85@gmail.com>
2022-06-05 11:20:19 -0400

    Added to the view.py file

--- to_do/views.py
+++ to_do/views.py
@@ -1,2 +1,8 @@
 from rest_framework.decorators import api_view
+from rest_framework import viewsets
+from .serializers import To_DoSerializer
+from .models import To_Do
 
+class To_DoViewSet(viewsets.ModelViewSet):
+    serializer_class = To_DoSerializer
+    queryset = To_Do.objects.all()
