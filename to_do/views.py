8ad72c623f06969ecd3aedd239ee9b5866d10fbd
Justin Goncalves <jvgoncalves85@gmail.com>
2022-06-12 15:57:35 -0400

    modified: to_do/views.py

--- to_do/views.py
+++ to_do/views.py
@@ -1,7 +1,12 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import To_DoSerializer
+from .models import To_Do
 
 @api_view(['GET'])
 def do_to_list(request):
+    todo = To_Do.objects.all()
 
-    return Response('ok')
+    serializer = To_DoSerializer(todo, many=True)
+
+    return Response(serializer.data) 
