b1050c24caead05c97d64a20a0d6cf977bf8fcaf
Justin Goncalves <jvgoncalves85@gmail.com>
2022-07-14 11:16:01 -0400

    Added a backend folder

--- To-Do-Backend/To_Do_Project/settings.py
+++ To-Do-Backend/To_Do_Project/settings.py
@@ -46,7 +46,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware'
 ]
 
 ROOT_URLCONF = 'To_Do_Project.urls'
@@ -116,3 +116,4 @@ try:
 except ImportError:
     pass
     
+CORS_ORIGIN_ALLOW_ALL = True
